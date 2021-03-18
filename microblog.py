940c3e2f18c2a5fcfea80003574584d512c01569
MatheusNinuma <mn.med18@uea.edu.br>
2021-03-18 18:09:05 +0000

    Hello World

---
+++ microblog.py
@@ -0,0 +1,9 @@
+from flask import Flask #Declaração importante
+
+app = Flask("microblog") #Criando o microblog
+
+@app.route("/") #Quando alguém acessar o URL, então ele roda o index a rota principal
+def index(): #Qualquer nome funciona
+    return "Olá, mundo!"
+
+app.run() #Rodar o programa
